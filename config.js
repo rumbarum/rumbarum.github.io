1700a49134e0a4824856af09d78cce5bc6863d12
Bareum Han <rumbarum@gmail.com>
2019-08-07 10:59:40 +0900

    19.08.07

--- config.js
+++ config.js
@@ -3,31 +3,31 @@
 module.exports = {
   url: 'https://rumbarum.github.io/',
   pathPrefix: '/',
-  title: 'rumbarum 개발세발',
+  title: 'rumbarum 개발세발개발',
   subtitle: '개발자 성장일기',
   copyright: '© All rights reserved.',
   disqusShortname: 'rumbarum',
   postsPerPage: 5,
   googleAnalyticsId: '',
-  useKatex: false,
+  useKatex: true,
   menu: [
     {
       label: 'Articles',
       path: '/'
     },
     {
-      label: 'About me',
+      label: '바름이는 누구?',
       path: '/pages/about'
     },
     {
-      label: 'Contact me',
+      label: '연락이 필요하시다면',
       path: '/pages/contacts'
     }
   ],
   author: {
     name: 'Bareum Han',
     photo: '/media/rumbarum.png',
-    bio: '개발세발 개발로 세상을 좀더 발전시켜보자!',
+    bio: '개발로 세상을 발전시키는 개발!',
     contacts: {
       email: 'rumbarum@gmail.com',
       telegram: '#',
--- content/pages/about.md
+++ content/pages/about.md
@@ -3,6 +3,10 @@ title: "바름바른 바름"
 template: "page"
 ---
 
-한바르으으으으음 
+한바른, 한빠른, 한발음 하는 한바름입니다.
 
-![Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.](/media/rumbarum.jpg)
+![Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.](/media/rumbarum.png)
+
+현재 코딩 부트 캠프 'WeCode' 에서 웹 개발 수련중입니다.
+
+취미는 인물 그리기, 트레바리 놀러가기, 그 외 기타  
